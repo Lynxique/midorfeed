356da39f48d2030b53d0af5ea5fd8ed6ce973e01
Karel Formánek <kapijacz@gmail.com>
2018-06-27 11:44:47 +0200

    Zmenil jsem Vypujcku v Zakaznika v Predmet

--- Knihovna/src/main/java/cz/jcu/uai/knihovna/Predmet.java
+++ Knihovna/src/main/java/cz/jcu/uai/knihovna/Predmet.java
@@ -14,6 +14,6 @@ import java.util.List;
 public abstract class Predmet {
     private String nazev;
     private int pocet;
-    private List<Vypujcka> vypujcky;
+    private List<Zakaznik> vypujcky;
     
 }
