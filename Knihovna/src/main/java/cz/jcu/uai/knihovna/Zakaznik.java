96ed0859a68376650142c93f3787232662c15dfa
hrombas <hrombas@seznam.cz>
2018-06-28 18:12:01 +0200

    něco málo
    Začátek api,
    okomentování Zakaznik

--- Knihovna/src/main/java/cz/jcu/uai/knihovna/Zakaznik.java
+++ Knihovna/src/main/java/cz/jcu/uai/knihovna/Zakaznik.java
@@ -8,23 +8,36 @@ package cz.jcu.uai.knihovna;
 import java.time.LocalDate;
 import java.io.Serializable;
 /**
- *
+ * Třída Zakaznik reprezentuje jednu výpůjčku,
+ * obsahuje jméno a datum výpůjčky a metody k získání 
+ * jména a výpůjční lhůty
  * @author User
  */
 public class Zakaznik implements Serializable {
-    private String jmeno;
-    private LocalDate lhutaOd, lhutaDo;
+    private final String jmeno;
+    private final LocalDate lhutaOd;
 
-    public Zakaznik (String jmeno, LocalDate lhutaOd, LocalDate lhutaDo) {
+    /**
+     * konstruktor Zakaznik
+     * @param jmeno String jméno výpůjčitele
+     * @param lhutaOd LocalDate datum výpůjčky
+     */
+    public Zakaznik (String jmeno, LocalDate lhutaOd) {
         this.jmeno = jmeno;
         this.lhutaOd = lhutaOd;
-        this.lhutaDo = lhutaDo;
     }
-    
+    /**
+     * metoda pro vyvolání jména výpujčitele,
+     * použitá pro umožnění vyhledávání podle výpůjčitele
+     * @return String jméno výpujčitele
+     */
     public String getJmeno(){
         return jmeno;
     }
-    
+    /**
+     * textová reprezentace výpůjčky
+     * @return String "jméno | lhůta od - do"
+     */
     public String asString(){
         return (jmeno + " | " + lhutaOd.toString() + " - " + lhutaOd.plusMonths(1).toString());
     }
