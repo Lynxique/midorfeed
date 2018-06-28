d94b419a5bf71658b363a0504bc79a977d1e8b92
Lynxique <Lynxique@seznam.cz>
2018-06-28 10:59:00 +0200

    kniha

--- Knihovna/src/main/java/cz/jcu/uai/knihovna/Predmet.java
+++ Knihovna/src/main/java/cz/jcu/uai/knihovna/Predmet.java
@@ -6,6 +6,13 @@
 package cz.jcu.uai.knihovna;
 
 import java.util.List;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 /**
@@ -13,9 +20,9 @@ import javafx.collections.ObservableList;
  * @author User
  */
 public abstract class Predmet {
-    private String nazev;
-    private int pocet;
-    private List<Zakaznik> vypujcky;
+    private StringProperty nazev;
+    private IntegerProperty pocet;
+    private ListProperty<Zakaznik> vypujcky;
     
     public void vypujc(Zakaznik zak) {
         getVypujcky().add(zak);
@@ -28,5 +35,17 @@ public abstract class Predmet {
     public ObservableList getVypujcky() {
         return vypujcky.get();
     }
+
+    public Predmet (String nazev, int pocet) {
+        this.nazev = new SimpleStringProperty(nazev);
+        this.pocet = new SimpleIntegerProperty(pocet);
+        ObservableList<Zakaznik> oL = FXCollections.observableArrayList();
+        this.vypujcky = new SimpleListProperty<Zakaznik>(oL);
+        
+    }
     
+    public Predmet (String nazev, int pocet, List vypujcky) {
+        this(nazev, pocet);
+        this.vypujcky.addAll(vypujcky);
+    }      
 }
