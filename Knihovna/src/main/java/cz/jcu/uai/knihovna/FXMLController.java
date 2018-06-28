2ce59178075be8b1b3762e11d4645f7603131902
Karel Formánek <Kapijacz@gmail.com>
2018-06-28 23:57:26 +0200

    GUI
    základ gui

--- Knihovna/src/main/java/cz/jcu/uai/knihovna/FXMLController.java
+++ Knihovna/src/main/java/cz/jcu/uai/knihovna/FXMLController.java
@@ -2,11 +2,9 @@ package cz.jcu.uai.knihovna;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
